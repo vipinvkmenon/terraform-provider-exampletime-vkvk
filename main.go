871feef2610c3c3ef6dc9dbdc50e1e9e65cf9fdf
Vipin Vijaykumar <vipin.vijaykumar@sap.com>
2024-05-03 20:02:04 +0530

    Second commit

--- main.go
+++ main.go
@@ -1,5 +1,12 @@
-// Copyright (c) HashiCorp, Inc.
-// SPDX-License-Identifier: MPL-2.0
+// Run "go generate" to format example terraform files and generate the docs for the registry/website
+
+// If you do not have terraform installed, you can remove the formatting command, but its suggested to
+// ensure the documentation is formatted properly.
+//go:generate terraform fmt -recursive ./examples/
+
+// Run the docs generation tool, check its repository for more information on how it works and how docs
+// can be customized.
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate -provider-name terraform-provider-exampletime-vkvk
 
 package main
 
